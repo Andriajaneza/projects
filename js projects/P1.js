f3e2ccab21cc1888c35a712ac54e5838eff766cd
Andria janezashvili <andriajaneza@gmail.com>
2024-12-04 20:48:52 +0400

    goa

--- js projects/P1.js
+++ js projects/P1.js
@@ -1,27 +1,29 @@
+// project #1 calculator
+
 var num1 = (
-                '2'   //<-- enter first number here
+                '1'   //<-- enter first number here
 )
 var opperator = (
                 '+'   //<-- enter opperator here
 )
 
 var num2 = (
-                '2'   //<-- enter second number here
+                '1'   //<-- enter second number here
 )
 
 var A = (parseFloat(num1))
 var B = (parseFloat(num2))
 
-if (opperator === '+') {
+if (opperator == '+') {
     console.log(A + B)
 }
-else if (opperator === '-') {
+else if (opperator == '-') {
     console.log(A - B)
 }
-else if (opperator === '/') {
+else if (opperator == '/') {
     console.log(A / B)
 }
-else if (opperator === '*') {
+else if (opperator == '*') {
     console.log(A * B)
 }
 else {
---
+++ js projects/P2.js
@@ -0,0 +1,25 @@
+// project #2 hello types
+
+A = (
+            'დავით' // <-- enter your name
+)
+
+B = (
+            '1' // <-- enter number 1 - 5
+)
+
+if (B == 1) {
+    console.log('გამარჯობა ' + A)
+}
+if (B == 2) {
+    console.log('ზდაროვა ' + A)
+}
+if (B == 3) {
+    console.log('ბარო ' + A)
+}
+if (B == 4) {
+    console.log('გაუმარჯოს ' + A)
+}
+if (B == 5) {
+    console.log('პრივეტ ' + A)
+}
